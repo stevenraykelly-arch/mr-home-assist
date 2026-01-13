fb2cfd31a36121d3093dea6f268261b3bd715d1e
stevenraykelly-arch <stevenraykelly@gmail.com>
2026-01-14 04:38:19 +1100

    fix: disable sitemap to unblock build and deploy location pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://mrhomeassist.com.au',
-    integrations: [tailwind(), sitemap()],
+    integrations: [tailwind() /*, sitemap() */],
     server: {
         host: true
     },
